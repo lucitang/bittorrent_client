f3648e482cc899c7a30c1622d5598872fd47d266
Sebastien Hideux <hideux.sebastien@gmail.com>
2024-10-25 14:57:27 +0200

    feat: added Extensions handshake

--- src/utils/trackers.rs
+++ src/utils/trackers.rs
@@ -40,7 +40,7 @@ pub struct QueryParams {
 pub struct TrackerResponse {
     /// An integer, indicating how often your client should make a request to the tracker.
     /// You can ignore this value for the purposes of this challenge.
-    pub interval: u64,
+    pub interval: Option<u64>,
 
     /// A string, which contains list of peers that your client can connect to.
     /// Each peer is represented using 6 bytes. The first 4 bytes are the peer's IP address and the last 2 bytes are the peer's port number.
